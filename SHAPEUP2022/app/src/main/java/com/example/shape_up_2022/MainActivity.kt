eacfc88e5214bdf73e42422b20680d60b9424a9e
unknown <lerle144@gmail.com>
2022-04-07 19:08:25 +0900

    프로젝트 베이스

---
+++ SHAPEUP2022/app/src/main/java/com/example/shape_up_2022/MainActivity.kt
@@ -0,0 +1,34 @@
+package com.example.shape_up_2022
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.shape_up_2022.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        // 혜수
+
+
+
+        // 수연
+
+
+
+        // 영주
+
+
+
+        // 성민
+
+        
+
+    }
+
+
+}
