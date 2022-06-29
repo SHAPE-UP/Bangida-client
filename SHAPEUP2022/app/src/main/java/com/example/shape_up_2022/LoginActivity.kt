40ec89c4c41c2995a9383dbd3cacddb218e7c7a4
SOOYEON YOO <dbtndus0527@gmail.com>
2022-06-29 22:22:29 +0900

    회원가입, 로그인, 로그아웃, 자동 로그인 (#32)
    * #31 회원가입 요청 코드 작성(초안)
    
    * #31 회원가입, 로그인, 로그아웃, 자동 로그인 코드 작성
    
    Co-authored-by: unknown <lerle144@gmail.com>

--- SHAPEUP2022/app/src/main/java/com/example/shape_up_2022/LoginActivity.kt
+++ SHAPEUP2022/app/src/main/java/com/example/shape_up_2022/LoginActivity.kt
@@ -3,9 +3,13 @@ package com.example.shape_up_2022
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
-import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.example.shape_up_2022.databinding.SignInBinding
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
+
 
 class LoginActivity : AppCompatActivity() {
 
@@ -19,38 +23,39 @@ class LoginActivity : AppCompatActivity() {
         binding.gotoSignUp.setOnClickListener{
             val intent = Intent(this, JoinActivity::class.java)
             startActivity(intent)
-            this.finish()
+            finish()
         }
 
         // 로그인 버튼을 눌렀을 때
         binding.btnLogin.setOnClickListener{
             val inputID = binding.inputId.text.toString() // 아이디에 대한 String
             val inputPW = binding.inputPw.text.toString() // 비밀번호에 대한 String
-            
-            // DB에서 아이디가 있는지 검색, 아이디가 있으면 아이디에 해당하는 비밀번호가 맞는지 검색
-            if (inputID == "" || inputPW == ""){ // ID or 패스워드를 입력하지 않았을 때
-                Log.d("app_test", "다시 입력하세요")
-                binding.reEnterIdPw.visibility = View.VISIBLE
-            }
-            else if (inputID == "1234"){ // 해당하는 아이디가 존재할 때
-                if(inputPW == "1234"){
-                    Log.d("app_test", "로그인 완료")
 
-                    // intent: mainActivity로 이동
-                    val intent = Intent(
-                        this, MainActivity::class.java)
-                    startActivity(intent)
-                    this.finish()
-                    // 로그인 이력 저장, 다음에는 로그인 창 없이 바로 메인 페이지로 이동하도록 구현
-                } else{ // 비밀번호가 틀린 경우
+            // DB에 저장된 아이디 값
+            val call: Call<LoginRes> = MyApplication.networkServiceAuth.login(
+                LoginReq(inputID, inputPW)
+            )
 
+            call?.enqueue(object : Callback<LoginRes> {
+                override fun onResponse(call: Call<LoginRes>, response: Response<LoginRes>) {
+                    if(response.isSuccessful){
+                        Log.d("mobileApp", "$response ${response.body()}")
+                        // 프리퍼런스에 값 저장
+                        SaveSharedPreference.setUserEmail(baseContext, inputID)
+
+                        // 메인 페이지로 이동
+                        val intent = Intent(baseContext, MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                    }
                 }
-            }
-            else{ // 아이디와 비밀번호가 틀린 경우
-                
-                Log.d("app_test", "로그인 실패 ${inputID}, ${inputPW}")
-                // 리렌더링 후 아이디와 비밀번호가 틀렸음을 알린다. (모달 띄우는 것보다는 xml에서 알리면 좋을 듯)
-            }
+
+                override fun onFailure(call: Call<LoginRes>, t: Throwable) {
+                    Log.d("mobileApp", "onFailure $t")
+                    Toast.makeText(baseContext, "아이디 또는 비밀번호를 확인해주세요.", Toast.LENGTH_SHORT).show()
+                }
+            })
+
         }
             //간편 로그인 연동 => api
     }
