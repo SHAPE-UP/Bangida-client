a9e28c2dc0ad9b87f545560c70e643788f65caca
unknown <lerle144@gmail.com>
2022-08-04 01:36:46 +0900

    #8 액티비티 추가(성향 점검 테스트, 시뮬레이션 시작, 견종 선택)

---
+++ SHAPEUP2022/app/src/main/java/com/example/shape_up_2022/SimStartActivity.kt
@@ -0,0 +1,58 @@
+package com.example.shape_up_2022
+
+import android.content.DialogInterface
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AlertDialog
+import com.example.shape_up_2022.databinding.ActivitySimStartBinding
+import com.example.shape_up_2022.databinding.BudgetEditBinding
+
+class SimStartActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding = ActivitySimStartBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        /* 테스트하지 않은 사용자를 돌려보내는 다이얼로그 */
+        // 취소(메인으로), 테스트(테스트 시작)
+        val eventhandler = object : DialogInterface.OnClickListener {
+            override fun onClick(p0: DialogInterface?, p1: Int) {
+                if(p1==DialogInterface.BUTTON_POSITIVE) {
+                    Log.d("mobileApp", "positive button")
+                }
+                else if (p1==DialogInterface.BUTTON_NEGATIVE) {
+                    val intent = Intent(this@SimStartActivity, MainActivity::class.java)
+                    startActivity(intent)
+                    finish()
+                }
+            }
+        }
+        var builder = AlertDialog.Builder(this)
+            .setTitle("테스트하지 않은 사용자")
+            .setMessage("반려견 케어 성향 점검을 먼저 진행하세요.")
+            .setPositiveButton("테스트", eventhandler)
+            .setNegativeButton("취소", eventhandler)
+            .setCancelable(false)
+        // if 성향점검테스트를 하지 않은 유저라면 모달창 표시
+        if (true) {
+            builder.show()
+        }
+
+
+
+        // 체크박스를 모두 체크해야 이동 버튼 활성화
+
+
+
+        // 입양하기 버튼 -> 화면 이동
+        binding.btnSimulationStartStart.setOnClickListener {
+            val intent = Intent(this, SimStartBreedActivity::class .java)
+            startActivity(intent)
+            finish()
+        }
+
+    }
+}
