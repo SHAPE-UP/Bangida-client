a7c8a10a72d96dbcd272baf0b0eacb48f2182a30
SOOYEON YOO <dbtndus0527@gmail.com>
2022-08-07 23:10:39 +0900

    리뷰 등록 UI 수정, 리뷰 등록 버튼에 클릭 이벤트 달기(intent 연결) (#67)
    * #35 산책 리뷰 등록(SimWalkReviewAddActivity.kt) UI 수정
    * #35 리뷰 저장하기 버튼에 클릭 이벤트 달기 / SimWalkMainActivity.kt 화면으로 이동, DB에 data 저장은 추후에 코드 작성 필요

--- SHAPEUP2022/app/src/main/java/com/example/shape_up_2022/SimWalkReviewAddActivity.kt
+++ SHAPEUP2022/app/src/main/java/com/example/shape_up_2022/SimWalkReviewAddActivity.kt
@@ -1,11 +1,25 @@
 package com.example.shape_up_2022
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.shape_up_2022.databinding.ActivitySimWalkReviewAddBinding
 
 class SimWalkReviewAddActivity : AppCompatActivity() {
+
+    lateinit var binding : ActivitySimWalkReviewAddBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding = ActivitySimWalkReviewAddBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_sim_walk_review_add)
+        setContentView(binding.root)
+
+        // 리뷰 저장
+        binding.addreviewSave.setOnClickListener {
+            // 리뷰 DB에 넘기기
+
+            // 산책 main 화면으로 이동
+            val intent = Intent(this, SimWalkMainActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
