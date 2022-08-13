c95e4dd2eb6702bee0bca5c751e488e6e986f7e0
hwangsungmin <hwangsungmin_00@naver.com>
2022-08-13 13:03:15 +0900

    메인페이지 수정

--- SHAPEUP2022/app/src/main/java/com/example/shape_up_2022/common/MainActivity.kt
+++ SHAPEUP2022/app/src/main/java/com/example/shape_up_2022/common/MainActivity.kt
@@ -1,14 +1,17 @@
 package com.example.shape_up_2022.common
 
+import android.content.DialogInterface
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import androidx.appcompat.app.AlertDialog
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.FragmentManager
 import androidx.fragment.app.FragmentTransaction
 import com.example.shape_up_2022.*
 import com.example.shape_up_2022.achieve.AchieveActivity
+import com.example.shape_up_2022.databinding.ActivityYoutubeBinding
 import com.example.shape_up_2022.databinding.MainPageBinding
 import com.example.shape_up_2022.simulation.SimulationActivity
 import com.example.shape_up_2022.simulation.TestActivity
@@ -22,6 +25,7 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(binding.root)
+        val bindingYouTube = ActivityYoutubeBinding.inflate(layoutInflater)
 
         // 강아지 현재 상태, 정보
 
@@ -61,8 +65,10 @@ class MainActivity : AppCompatActivity() {
             overridePendingTransition(0, 0)
             finish()
         }
-
         // 하단 메뉴
+        binding.menuBtn2.setOnClickListener {
+            videoGather()
+        }
         binding.menuBtn3.setOnClickListener {
             val intent = Intent(this, TestActivity::class.java)
             startActivity(intent)
@@ -75,6 +81,14 @@ class MainActivity : AppCompatActivity() {
             //finish()
         }
     }
+    private fun videoGather() = object : DialogInterface.OnClickListener{
+        override fun onClick(p0: DialogInterface?, p1: Int) {
+            val intent = Intent(this@MainActivity, YoutubeActivity::class.java)
+            intent.putExtra("search", "강아지 훈련")
+            startActivity(intent)
+        }
+    }
+
 
     // 프래그먼트 연결
     private fun viewFragment(fragment : Fragment, location:Int){
