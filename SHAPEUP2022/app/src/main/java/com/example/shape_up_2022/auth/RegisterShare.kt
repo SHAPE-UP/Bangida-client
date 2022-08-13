439058f8c1adb6182bd5bc26abb3e88d083a5e4e
unknown <lerle144@gmail.com>
2022-08-13 22:59:13 +0900

    #11 가족공유코드 요청 오류

--- SHAPEUP2022/app/src/main/java/com/example/shape_up_2022/auth/RegisterShare.kt
+++ SHAPEUP2022/app/src/main/java/com/example/shape_up_2022/auth/RegisterShare.kt
@@ -1,11 +1,20 @@
 package com.example.shape_up_2022.auth
 
+import com.example.shape_up_2022.common.SaveSharedPreference
 import android.os.Bundle
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import com.example.shape_up_2022.databinding.FragmentJoinShareBinding
+import com.example.shape_up_2022.retrofit.JoinFamilyReq
+import com.example.shape_up_2022.retrofit.JoinFamilyRes
+import com.example.shape_up_2022.retrofit.MyApplication
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 // TODO: Rename parameter arguments, choose names that match
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -38,7 +47,41 @@ class JoinShare : Fragment() {
         // 반려견 공유 코드를 입력하면 다음으로
         val binding = FragmentJoinShareBinding.inflate(inflater, container, false)
         binding.nextShare.setOnClickListener {
-            (activity as RegisterActivity).gotoMainActivity()
+
+            val familyCode: String = binding.familyCode.text.toString()
+            val userID: String = SaveSharedPreference.getUserID(requireActivity().applicationContext!!)!!
+
+            if(familyCode == "") {  // 입력한 코드가 없으면
+                //(activity as RegisterActivity).gotoMainActivity()  // 이거 말고 로그인 액티비티로
+            }
+
+            else {
+                val call: Call<JoinFamilyRes> = MyApplication.networkServiceAuth.joinFamliy(
+                    JoinFamilyReq(familyCode = familyCode, userID = userID)
+                )
+
+                call?.enqueue(object : Callback<JoinFamilyRes> {
+                    override fun onResponse(call: Call<JoinFamilyRes>, response: Response<JoinFamilyRes>) {
+                        if(response.isSuccessful){
+                            Log.d("mobileApp", "$response ${response.body()}")
+
+                            /*
+                            //////작업중
+                            if (리스폰스.success == true) {
+                                // 로그인 액티비티로 이동
+                            } else {  // success == false
+                                Toast.makeText(requireContext(), "올바르지 않은 코드입니다.", Toast.LENGTH_LONG)
+                            }
+                            */
+
+                        }
+                    }
+
+                    override fun onFailure(call: Call<JoinFamilyRes>, t: Throwable) {
+                        Log.d("mobileApp", "onFailure $t")
+                    }
+                })
+            }
         }
 
         return binding.root
@@ -63,6 +106,4 @@ class JoinShare : Fragment() {
                 }
             }
     }
-
-
 }
--- SHAPEUP2022/app/src/main/java/com/example/shape_up_2022/common/SaveSharedPreference.kt
+++ SHAPEUP2022/app/src/main/java/com/example/shape_up_2022/common/SaveSharedPreference.kt
@@ -4,13 +4,13 @@ import android.content.Context
 import android.content.SharedPreferences
 import androidx.preference.PreferenceManager
 
-class SaveSharedPreference {
+public class SaveSharedPreference {
     companion object{
         fun getSharedPreferences(ctx: Context): SharedPreferences? {
             return PreferenceManager.getDefaultSharedPreferences(ctx)
         }
 
-        // email
+        // User의 email
         fun setUserEmail(ctx: Context, email: String) {
             val editor = getSharedPreferences(ctx)!!.edit()
             editor.putString("useremail", email)
@@ -23,7 +23,7 @@ class SaveSharedPreference {
             editor.commit()
         }
 
-        // 닉네임
+        // User의 name
         fun setUserName(ctx: Context, name: String) {
             val editor = getSharedPreferences(ctx)!!.edit()
             editor.putString("username", name)
@@ -31,6 +31,32 @@ class SaveSharedPreference {
         }
         fun getUserName(ctx: Context?): String? { return getSharedPreferences(ctx!!)!!.getString("username", "") }
         fun clearUserName(ctx: Context?) {
+            val editor = getSharedPreferences(ctx!!)!!.edit()
+            editor.clear()
+            editor.commit()
+        }
+
+        // User의 _id
+        fun setUserID(ctx: Context, name: String) {
+            val editor = getSharedPreferences(ctx)!!.edit()
+            editor.putString("userID", name)
+            editor.commit()
+        }
+        fun getUserID(ctx: Context?): String? { return getSharedPreferences(ctx!!)!!.getString("userID", "") }
+        fun clearUserID(ctx: Context?) {
+            val editor = getSharedPreferences(ctx!!)!!.edit()
+            editor.clear()
+            editor.commit()
+        }
+
+        // Family의 _id
+        fun setFamliyID(ctx: Context, name: String) {
+            val editor = getSharedPreferences(ctx)!!.edit()
+            editor.putString("familyID", name)
+            editor.commit()
+        }
+        fun getFamliyID(ctx: Context?): String? { return getSharedPreferences(ctx!!)!!.getString("familyID", "") }
+        fun clearFamliyID(ctx: Context?) {
             val editor = getSharedPreferences(ctx!!)!!.edit()
             editor.clear()
             editor.commit()
