64e8ddb4f71e90ab33f5ed21f8d50f7956e8e4d9
unknown <lerle144@gmail.com>
2022-09-11 16:26:57 +0900

    #22 #21 가족 구성원 목록 프래그먼트 RecyclerView로 가로로 변경, 화면 디자인 조금 변경

--- SHAPEUP2022/app/src/main/java/com/example/shape_up_2022/common/FamilyFragment.kt
+++ SHAPEUP2022/app/src/main/java/com/example/shape_up_2022/common/FamilyFragment.kt
@@ -10,9 +10,15 @@ import android.widget.BaseAdapter
 import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.example.shape_up_2022.R
+import com.example.shape_up_2022.adapter.TodoViewHolder
+import com.example.shape_up_2022.data.TodoItem
 import com.example.shape_up_2022.data.User
 import com.example.shape_up_2022.databinding.FragmentFamilyBinding
+import com.example.shape_up_2022.databinding.ItemFamilyBinding
+import com.example.shape_up_2022.databinding.TodoitemBinding
 import com.example.shape_up_2022.retrofit.GetFamilyReq
 import com.example.shape_up_2022.retrofit.GetFamilyRes
 import com.example.shape_up_2022.retrofit.MyApplication
@@ -25,7 +31,7 @@ import retrofit2.Response
 // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
-
+/*
 class FamilyAdapter(private val items: MutableList<User>): BaseAdapter() {
 
     override fun getCount(): Int = items.size
@@ -50,6 +56,33 @@ class FamilyAdapter(private val items: MutableList<User>): BaseAdapter() {
         return convertView
     }
 }
+*/
+class FamilyViewHolder(val binding: ItemFamilyBinding): RecyclerView.ViewHolder(binding.root)
+class FamilyAdapter (val datas: MutableList<User>?): RecyclerView.Adapter<RecyclerView.ViewHolder>() {
+
+    override fun getItemCount(): Int {
+        return datas?.size ?: 0
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
+        return FamilyViewHolder(
+            ItemFamilyBinding.inflate(
+                LayoutInflater.from(parent.context),
+                parent,
+                false
+            )
+        )
+    }
+
+    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+        val binding = (holder as FamilyViewHolder).binding
+
+        //binding.fImage.setImageDrawable(datas!![position].image)
+        binding.fName.text = datas!![position].name
+
+    }
+}
+
 
 /**
  * A simple [Fragment] subclass.
@@ -60,6 +93,8 @@ class FamilyFragment : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
+    var familyArray = mutableListOf<User>()  // 가족 구성원 목록
+    lateinit var familyAdapter: FamilyAdapter
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -76,6 +111,13 @@ class FamilyFragment : Fragment() {
         // Inflate the layout for this fragment
         val binding = FragmentFamilyBinding.inflate(inflater, container, false)
 
+        // 리사이클러뷰 설정 및 초기화
+        val layoutManager = LinearLayoutManager(context)
+        layoutManager.setOrientation(LinearLayoutManager.HORIZONTAL)
+        binding.familyListView.layoutManager = layoutManager
+        familyAdapter = FamilyAdapter(familyArray)
+        binding.familyListView.adapter = familyAdapter
+
         val call: Call<GetFamilyRes> = MyApplication.networkServiceFamily.getFamily(
             GetFamilyReq(SaveSharedPreference.getFamliyID(context)!!)
         )
@@ -83,8 +125,10 @@ class FamilyFragment : Fragment() {
             override fun onResponse(call: Call<GetFamilyRes>, response: Response<GetFamilyRes>) {
                 if(response.isSuccessful){
                     //Log.d("mobileApp", "$response ${response.body()}")
-                    var userGroup = response.body()!!.family.userGroup.toMutableList()
-                    binding.familyListView.adapter = FamilyAdapter(userGroup)
+                    familyArray = response.body()!!.family.userGroup.toMutableList()
+                    familyAdapter = FamilyAdapter(familyArray)
+                    binding.familyListView.adapter = familyAdapter
+                    familyAdapter.notifyDataSetChanged()
                 }
             }
             override fun onFailure(call: Call<GetFamilyRes>, t: Throwable) {
